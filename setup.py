e188f9b915ec6940bea15c9a47d9be56e85f19a8
niveditapatel <66324301+niveditapatel@users.noreply.github.com>
2021-05-17 18:53:35 -0400

    added changes in functions

--- setup.py
+++ setup.py
@@ -1,26 +1,58 @@
 import setuptools
+from setuptools import setup
+import io
+import os
+import re
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
-    },
-    classifiers=[
+def read(path, encoding='utf-8'):
+    path = os.path.join(os.path.dirname(__file__), path)
+    with io.open(path, encoding=encoding) as fp:
+        return fp.read()
+
+def version(path):
+    version_file = read(path)
+    version_match = re.search(r"""^__version__ = ['"]([^'"]*)['"]""",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(name='pylenm',
+      version=version('pylenm/__init__.py'),
+      description='This package aims to ...',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/niveditapatel/py4openag',
+      author='Nivedita Patel',
+      author_email='patelnivedita@icloud.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      install_requires=[
+        'markdown',
+        'pandas',
+        'openpyxl',
+        'elevation',
+        'richdem',
+        'rasterio',
+        'numpy',
+        'seaborn',
+        'matplotlib',
+        'statsmodels',
+        'scipy',
+        'pyproj',
+        'datetime',
+        'sklearn',
+        'supersmoother',
+        'ipyleaflet>=0.13.0',
+        'ipywidgets>=7.5.0'
+        ],
+      classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
-)
+        ],
+      python_requires='>=3.6',
+      zip_safe=False)
---
+++ src/py4openag/__init__.py
@@ -0,0 +1,1 @@
+from .py4openag import functions
